dd807a512b5f0f64e5b784a4b1c3e4e4cfdfc565
Nour Moftah <noursaad2213@gmail.com>
2026-08-18 02:56:09 +0300

    Refactor SimilarAdCard component for improved readability and structure

--- src/components/similar-ads/similar-ad-card.tsx
+++ src/components/similar-ads/similar-ad-card.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import { FiThumbsUp } from "react-icons/fi";
 import { IoEyeSharp } from "react-icons/io5";
 
@@ -10,43 +10,92 @@ type SimilarAdCardProps = {
   advertisement: SimilarAdvertisementView;
 };
 
-export function SimilarAdCard({ advertisement }: SimilarAdCardProps) {
+export function SimilarAdCard({
+  advertisement,
+}: SimilarAdCardProps) {
   return (
     <article className="similarAdCard">
-      <Link href={advertisement.id} className="similarAdMedia" style={{ display: 'block' }}>
+      <Link
+        href={advertisement.id}
+        className="similarAdMedia"
+        aria-label={advertisement.title}
+      >
         {advertisement.thumbnail ? (
-          <Image alt={advertisement.title} fill sizes="(max-width: 620px) 84vw, (max-width: 960px) 42vw, 240px" src={advertisement.thumbnail} />
+          <Image
+            src={advertisement.thumbnail}
+            alt={advertisement.title}
+            fill
+            sizes="(max-width: 620px) 84vw, (max-width: 960px) 42vw, 240px"
+          />
         ) : (
-          <span aria-hidden="true" className="similarAdImageFallback" />
+          <span
+            aria-hidden="true"
+            className="similarAdImageFallback"
+          />
         )}
-        <span className="similarAdPlay">
+
+        <span
+          aria-hidden="true"
+          className="similarAdPlay"
+        >
           <Icon name="play" size={24} />
         </span>
-        <span className="similarAdBadge">{advertisement.badge}</span>
+
+        <span className="similarAdBadge">
+          {advertisement.badge}
+        </span>
       </Link>
+
       <div className="similarAdContent">
         <h3>{advertisement.title}</h3>
+
         <div className="similarAdStats">
           <span>
-            <span className="statIconBg"><FiThumbsUp size={14} /></span>
+            <span
+              aria-hidden="true"
+              className="statIconBg"
+            >
+              <FiThumbsUp size={14} />
+            </span>
+
             {advertisement.likes}
           </span>
+
           <span>
-            <span className="statIconBg"><IoEyeSharp size={14} /></span>
+            <span
+              aria-hidden="true"
+              className="statIconBg"
+            >
+              <IoEyeSharp size={14} />
+            </span>
+
             {advertisement.views}
           </span>
         </div>
+
         <div className="similarAdBrandPill">
           {advertisement.brandLogo ? (
-            <Image alt={advertisement.brandName} className="similarAdBrandLogo" height={18} width={18} src={advertisement.brandLogo} />
+            <Image
+              src={advertisement.brandLogo}
+              alt={advertisement.brandName}
+              width={18}
+              height={18}
+              className="similarAdBrandLogo"
+            />
           ) : (
-            <span className="similarAdBrandLogoFallback" />
+            <span
+              aria-hidden="true"
+              className="similarAdBrandLogoFallback"
+            />
           )}
+
           <span>{advertisement.brandName}</span>
         </div>
-        <p className="similarAdExcerpt">{advertisement.excerpt}</p>
+
+        <p className="similarAdExcerpt">
+          {advertisement.excerpt}
+        </p>
       </div>
     </article>
   );
 }
-
