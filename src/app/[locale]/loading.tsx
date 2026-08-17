a956b84e2c3c7ce3372322f91db5b0e07d863cdd
Nour Moftah <noursaad2213@gmail.com>
2026-08-18 02:32:15 +0300

    Add Loading component for displaying loading state with accessibility attributes

---
+++ src/app/[locale]/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <main className="pageLoading" aria-busy="true" aria-live="polite">
+      <span />
+      <span />
+      <span />
+    </main>
+  );
+}
